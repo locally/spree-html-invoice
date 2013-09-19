cb1ef7c342741c7fb0ca9958a10acbb7304e3436
dcsmith130 <dan@locally.co>
2013-09-19 13:23:11 -0400

    initial commit for locally changes.

--- app/overrides/add_html_invoice_buttons.rb
+++ app/overrides/add_html_invoice_buttons.rb
@@ -1,5 +1,6 @@
+Spree::HtmlInvoice::Config.set(:print_buttons => "packaging_slip")
 Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
                      :name => "add_html_invoice_buttons",
-                     :insert_after => ".sidebar",
+                     :insert_after => "[data-hook=admin_order_tabs]",
                      :partial => "spree/admin/orders/html_buttons",
                      :disabled => false)
